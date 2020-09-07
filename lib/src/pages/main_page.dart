8ef2d39522b130025310d2c850852c3608878339
Dani <daniel.benet@outlook.es>
2020-09-07 17:25:41 +0200

    bubbles added

--- lib/src/pages/main_page.dart
+++ lib/src/pages/main_page.dart
@@ -13,54 +13,59 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  Widget categoryBubble() {
-    return Container(
-        child: Row(
-      children: [
-        Text("Hello"),
-        Text("Hello"),
-        Text("Hello"),
-        Text("Hello"),
-        Text("Hello"),
-        Text("Hello"),
-        Text("Hello"),
-        Text("Hello"),
-        Text("Hello"),
-        Text("Hello"),
-        Text("Hello"),
-        Text("Hello"),
-      ],
-    ));
+  Widget categories() {
+    return Expanded(
+      child: new ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 8,
+        itemBuilder: (context, index) => new Stack(
+          alignment: Alignment.topLeft,
+          children: [
+            new Container(
+              width: 60.0,
+              height: 60.0,
+              decoration: new BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: new DecorationImage(
+                      image: new NetworkImage(
+                          'https://www.google.es/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'),
+                      fit: BoxFit.fill)),
+              margin: const EdgeInsets.symmetric(horizontal: 8.0),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget searchBar() {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: SearchBar<Post>(
+          searchBarStyle: SearchBarStyle(
+            backgroundColor: Theme1.gray,
+            padding: EdgeInsets.only(top: 0.75, left: 20),
+            borderRadius: BorderRadius.circular(100),
+          ),
+          onSearch: search,
+          onItemFound: (Post post, int index) {
+            return ListTile(
+              title: Text(post.title),
+              subtitle: Text(post.description),
+            );
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: SearchBar<Post>(
-                searchBarStyle: SearchBarStyle(
-                  backgroundColor: Theme1.gray,
-                  padding: EdgeInsets.only(top: 0.75, left: 20),
-                  borderRadius: BorderRadius.circular(100),
-                ),
-                onSearch: search,
-                onItemFound: (Post post, int index) {
-                  return ListTile(
-                    title: Text(post.title),
-                    subtitle: Text(post.description),
-                  );
-                },
-              ),
-            ),
-          ),
-          //categoryBubble(),
-        ],
-      ),
-    );
+        body: Column(
+      children: <Widget>[categories()],
+    ));
   }
 }
 
